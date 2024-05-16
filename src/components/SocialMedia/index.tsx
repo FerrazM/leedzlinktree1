9bb4c7aeab91d39ff90a0495004e3c6f484a2212
FerrazM <matheusferrazo_@hotmail.com>
2024-05-16 09:39:14 -0300

    Adicionando Target Blank nos links

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -4,10 +4,10 @@ import { LayoutLinks, StyledLink } from "./styles"
 const SocialMedia = () => {
     return (
         <LayoutLinks>
-            <StyledLink href="https://www.instagram.com/agencianewsalles/">Instagram</StyledLink>
-            <StyledLink href="https://wa.me/5511998231157">WhatsApp</StyledLink>
-            <StyledLink href="tel:5511998231157">Telefone</StyledLink>
-            <StyledLink href="https://newsalles.com.br/">Acesse nosso site</StyledLink>
+            <StyledLink  target="_blank" href="https://www.instagram.com/agencianewsalles/">Instagram</StyledLink>
+            <StyledLink target="_blank" href="https://wa.me/5511998231157">WhatsApp</StyledLink>
+            <StyledLink target="_blank" href="tel:5511998231157">Telefone</StyledLink>
+            <StyledLink target="_blank" href="https://newsalles.com.br/">Acesse o nosso site</StyledLink>
         </LayoutLinks>
     )     
 }
